7b9d8f0847af504dc35437533892a7fb52e8ca0a
mik9016 <mikolajgruszecki@o2.pl>
2021-01-23 21:40:25 +0100

    connecting with MongoDB and creating controller and models

---
+++ controllers/newsletters.js
@@ -0,0 +1,65 @@
+const Newsletter = require('../models/Newsletter');
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+const EMAIL = process.env.EMAIL;
+const PASSWORD = process.env.PASSWORD;
+
+exports.findNewsletters =  async (req, res) => {
+    const newsletter = await Newsletter.find();
+    res.send({data: newsletter});
+
+};
+
+exports.createNewsletter = async (req,res) => {
+   const newsletter = new Newsletter(req.body);
+   await newsletter.save();
+
+ 
+    //cashing data 
+    let lastItem = req.body.email;
+    console.log(lastItem)
+    
+    //NODEMAILER
+
+    const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: EMAIL,
+        pass: PASSWORD
+    }
+});
+
+    const mailOptions = { 
+        from: EMAIL,
+        to: lastItem,
+        subject: 'Your Fresh Design News!',
+        text: 'WELCOME TO THE NEWSLETTER!!!!'
+    }
+    
+    transporter.sendMail(mailOptions,(error,info) => {
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Email sent: ' + info.response)
+        }
+    });
+
+    res.send({
+        status: true,
+        message: 'Data is received',
+    });
+//    res.send({data: newsletter});
+};
+
+exports.findNewsletter = async (req,res) => {
+    try{
+        const newsletter = await Newsletter.findById(req.params.id);
+        res.send({data: newsletter});
+        
+    } catch{
+        res.status(404).send({error: 'Newsletter not found'});
+    }
+    
+   
+}
--- server.js
+++ server.js
@@ -3,93 +3,107 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
-
-//DB
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = process.env.URI;
-// const client = new MongoClient(uri, { useNewUrlParser: true },{ useUnifiedTopology: true }, () => {
-//     console.log('connected')
-// });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// })
-
+require('dotenv').config();
+const mongoose = require('mongoose');
+const newsletterController = require('./controllers/newsletters');
 
 
 const app = express();
 
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-require('dotenv').config();
+
 //definig 
-const EMAIL = process.env.EMAIL;
-const PASSWORD = process.env.PASSWORD;
-const postArr = [];
 
-//NODEMAILER
+const URI = process.env.URI;
 
-const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: EMAIL,
-        pass: PASSWORD
-    }
-})  
+
+
+//MONGOOSE
+
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+
+console.log('Connected to DB!')
+// app.use(express.json());
+
+app.get('/newsletter', newsletterController.findNewsletters);
+app.post('/newsletter', newsletterController.createNewsletter);
+app.get('/newsletter/:id', newsletterController.findNewsletter);
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}.`)}
+
+);
+
+}).catch((err) => {
+    console.log(err);
+})
+
+
+
+
+
+
+
+
+
+
 
 
 //REQUESTS
 
-app.get('/',(req,res)=> {
-    res.send({
-        message: ' your get req'
+// app.get('/',(req,res)=> {
+//     res.send({
+//         message: ' your get req'
 
-    })
-} )
+//     })
+// } )
 
-app.post('/',(req, res) => {
-    //pushing data from client to postArr
-    postArr.push(req.body)
-    postArr.forEach(element => {
-        console.log(element)
-    });
-    //cashing data 
-    let lastItem = postArr[postArr.length-1];
-    console.log(lastItem.email)
+// app.post('/',(req, res) => {
+    // //pushing data from client to postArr
+    // postArr.push(req.body)
+    // postArr.forEach(element => {
+    //     console.log(element)
+    // });
+    // //cashing data 
+    // let lastItem = postArr[postArr.length-1];
+    // console.log(lastItem.email)
     
 
-    const mailOptions = { 
-        from: EMAIL,
-        to: lastItem.email,
-        subject: 'Your Fresh Design News!',
-        text: 'WELCOME TO THE NEWSLETTER!!!!'
-    }
+    // const mailOptions = { 
+    //     from: EMAIL,
+    //     to: lastItem.email,
+    //     subject: 'Your Fresh Design News!',
+    //     text: 'WELCOME TO THE NEWSLETTER!!!!'
+    // }
     
-    transporter.sendMail(mailOptions,(error,info) => {
-        if(error){
-            console.log(error);
-        }else{
-            console.log('Email sent: ' + info.response)
-        }
-    });
+    // transporter.sendMail(mailOptions,(error,info) => {
+    //     if(error){
+    //         console.log(error);
+    //     }else{
+    //         console.log('Email sent: ' + info.response)
+    //     }
+    // });
 
-    res.send({
-        status: true,
-        message: 'Data is received',
-    });
-
-
-});
+    // res.send({
+    //     status: true,
+    //     message: 'Data is received',
+    // });
 
 
+// });
 
-app.listen(port, () => 
-  console.log(`App is listening on port ${port}.`)
-);
+
+
+
+// APP LISTENING PORT
+// app.listen(port, () => {
+//     console.log(`App is listening on port ${port}.`)}
+
+// );
