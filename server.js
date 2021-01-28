395ba364e8aadbff24374ac2d04384ee73150c78
mik9016 <mikolajgruszecki@o2.pl>
2021-01-28 17:01:01 +0100

    adding port env

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const newsletterController = require('./controllers/newsletters');
 const app = express();
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
